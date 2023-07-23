12de412c065527346f48fcd56cb6891811f07e71
maybe-lu <2718428207@qq.com>
2023-07-23 13:21:20 +0800

    text

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gee_web
 
 go 1.18
 
+require gee v0.0.0-00010101000000-000000000000
+
 replace gee => ./gee
+
+
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"gee"
+	"log"
+	"net/http"
+	"time"
+)
+
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+
+		t := time.Now()
+
+		c.Fail(500, "Internal Server Error")
+
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
-	fmt.Print("a ...any")
+	r := gee.New()
+	r.Use(gee.Logger())
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+
+	r.Run(":8080")
 }
